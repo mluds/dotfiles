4092913e8999e4228eea18265df63ae9b1c55249
Mike Ludwig <mludwig@mitre.org>
2017-08-31 21:17:34 -0400

    Switch to vim-plug. Add auto install for it.

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # Get dotfiles directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/dotfiles"
 
+# Install dotfiles
 for name in $(ls $DIR); do
   ln -sf $DIR/$name ~/.$name
 done
+
+# Install vim-plug
+curl -fLso ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
