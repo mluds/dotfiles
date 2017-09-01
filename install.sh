061b698b3fbd27df2736e010d631a7ad38219d6e
Mike Ludwig <mludwig@mitre.org>
2017-09-01 13:10:17 +0000

    Add vim-plug and NERDTree

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/dotfiles"
 for name in $(ls $DIR); do
   ln -sf $DIR/$name ~/.$name
 done
+
+curl -fLso ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
