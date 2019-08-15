273220433794407326dbb5f9227bedf79709c461
Mike Ludwig <mludwig@mitre.org>
2019-08-15 19:54:48 +0200

    fix tab/space mix

--- install.sh
+++ install.sh
@@ -26,22 +26,22 @@ IFS=$OLDIFS
 
 # OS packages
 if [ $ID == "centos" ]; then
-	curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
-	sudo yum install -y \
-		epel-release \
-		http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
-	sudo yum install -y \
-		ack cowsay tmux zsh git xorg-x11-server-utils \
-		automake gcc gcc-c++ kernel-devel cmake \
-		python-devel python3-devel \
-		nodejs
+    curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
+    sudo yum install -y \
+        epel-release \
+        http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
+    sudo yum install -y \
+        ack cowsay tmux zsh git xorg-x11-server-utils \
+        automake gcc gcc-c++ kernel-devel cmake \
+        python-devel python3-devel \
+        nodejs
 elif [ $ID == "ubuntu" ]; then
-	curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-	sudo apt update && sudo apt install -y \
-		ack cowsay tmux zsh git x11-xserver-utils \
-		automake gcc g++ linux-headers-generic cmake \
-		python-dev python-pip python3-dev \
-		nodejs
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt update && sudo apt install -y \
+        ack cowsay tmux zsh git x11-xserver-utils \
+        automake gcc g++ linux-headers-generic cmake \
+        python-dev python-pip python3-dev \
+        nodejs
 fi
 
 # pyenv
@@ -53,12 +53,12 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # install vim-plug and plugins
 curl -fLso ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +GoInstallBinaries +qall
 
 # install YCM
 ~/.vim/plugged/youcompleteme/install.py \
-	--clang-completer \
-	--js-completer
+    --clang-completer \
+    --js-completer
 
 sudo python -m pip install flake8
